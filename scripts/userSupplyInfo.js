662a808328476459cb460b849408f8540f25e676
Vishakha Bansal <vishakhabansal16@gmail.com>
2023-04-26 14:44:26 +0530

    Modified schemas and routes

--- scripts/userSupplyInfo.js
+++ scripts/userSupplyInfo.js
@@ -18,15 +18,18 @@ export const initSupplyInfo = async (account) => {
   const resultantObject = {};
   const asset1 = "0xAAD4992D949f9214458594dF92B44165Fb84dC19";
   const array1 = await implementationContract.userTokenInfo(asset1, account);
-  resultantArray.push(array1[1]);
+  const supplyAmount1 = parseInt(array1[1], 10);
+  resultantArray.push(supplyAmount1);
 
   const asset2 = "0x42a71137C09AE83D8d05974960fd607d40033499";
   const array2 = await implementationContract.userTokenInfo(asset2, account);
-  resultantArray.push(array2[1]);
+  const supplyAmount2 = parseInt(array2[1], 10);
+  resultantArray.push(supplyAmount2);
 
   const asset3 = "0xaf95Ff5fB592646D86BF240B3CaE0903b6E4dd38";
   const array3 = await implementationContract.userTokenInfo(asset3, account);
-  resultantArray.push(array3[1]);
+  const supplyAmount3 = parseInt(array3[1], 10);
+  resultantArray.push(supplyAmount3);
 
   resultantObject.wbtcSupplyAmount = resultantArray[0];
   resultantObject.wethSupplyAmount = resultantArray[1];
